1595c92b3b1a0fbdcd94fac849fc078fe14a198d
Alexander Burkov <alexander.burkov@jetbrains.com>
2022-08-15 17:52:48 +0300

    Remove spring

--- server/src/main/kotlin/com/github/burkov/rentalfinder/entity/AdvertismentDao.kt
+++
@@ -1,7 +0,0 @@
-package com.github.burkov.rentalfinder.entity
-
-import java.math.BigDecimal
-
-class AdvertisementDao(
-    val price: BigDecimal
-)
--- server/src/main/kotlin/com/github/burkov/rentalfinder/repository/RentalRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.github.burkov.rentalfinder.repository
-
-import com.github.burkov.rentalfinder.entity.RentalDao
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface RentalRepository : JpaRepository<RentalDao, String>
